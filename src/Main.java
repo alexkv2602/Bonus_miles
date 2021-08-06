ceb765912d1de1e782c7e6bce0c930aee7ecb1c3
Alex Kleschenko <alexkv2694@gmail.com>
2021-08-06 14:00:02 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 10500;
+        int bonus = 20;
+        int result = (ticketPrice) / (bonus);
+        System.out.println(result + " miles");
+
+    }
+}
